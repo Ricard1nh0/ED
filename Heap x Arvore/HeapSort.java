43ba36e24cc7be0d5f5987800426e6744bc76176
Ricard1nh0 <128528777+Ricard1nh0@users.noreply.github.com>
2024-08-01 17:02:53 -0300

    Update HeapSort.java

--- Heap x Arvore/HeapSort.java
+++ Heap x Arvore/HeapSort.java
@@ -9,51 +9,41 @@ public class HeapSort {
     public void sort(int arr[]) {
         int n = arr.length;
 
-        // Constrói o heap (reorganiza o array)
         for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
 
-        // Extrai um elemento do heap de cada vez
         for (int i = n - 1; i > 0; i--) {
-            // Move a raiz atual para o fim
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
 
-            // Chama heapify na heap reduzida
             heapify(arr, i, 0);
         }
     }
 
-    // Para reorganizar uma sub-árvore em um heap
     void heapify(int arr[], int n, int i) {
-        int largest = i; // Inicializa o maior como raiz
-        int left = 2 * i + 1; // esquerda = 2*i + 1
-        int right = 2 * i + 2; // direita = 2*i + 2
+        int largest = i; 
+        int left = 2 * i + 1; 
+        int right = 2 * i + 2; 
 
-        // Se o filho da esquerda é maior que a raiz
         if (left < n && arr[left] > arr[largest]) {
             largest = left;
         }
 
-        // Se o filho da direita é maior que o maior até agora
         if (right < n && arr[right] > arr[largest]) {
             largest = right;
         }
 
-        // Se o maior não é a raiz
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
 
-            // Recursivamente heapify a sub-árvore afetada
             heapify(arr, n, largest);
         }
     }
 
-    // Método utilitário para imprimir o array
     static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i) {
@@ -62,7 +52,6 @@ public class HeapSort {
         System.out.println();
     }
 
-    // Método para ler um arquivo de texto e converter em um array de inteiros
     static int[] readArrayFromFile(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line = br.readLine();
@@ -78,9 +67,7 @@ public class HeapSort {
         }
     }
 
-    // Método principal para testar o código
     public static void main(String args[]) {
-        // Define o nome do arquivo diretamente no código
         String filename = "dados500_mil.txt";
         
         int arr[] = readArrayFromFile(filename);
