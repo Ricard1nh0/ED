7dd189cb3aa7e32ab82ab3f1931a1c463179726c
Ricard1nh0 <128528777+Ricard1nh0@users.noreply.github.com>
2024-08-29 19:05:56 -0300

    Update Main.java

--- AVL/Main.java
+++ AVL/Main.java
@@ -5,23 +5,19 @@ public class Main {
         AVLTree arvore = new AVLTree();
         Random random = new Random();
 
-        // Inserindo 100 números aleatórios na árvore AVL
         for (int i = 0; i < 100; i++) {
             int numero = random.nextInt(1001) - 500; // Números de -500 a 500
             arvore.inserir(numero);
         }
 
-        // Verificando o fator de balanceamento de cada nó após as inserções
         System.out.println("Árvore após inserções:");
         arvore.imprimirFatoresBalanceamento();
 
-        // Removendo 20 números aleatórios da árvore
         for (int i = 0; i < 20; i++) {
             int numero = random.nextInt(1001) - 500;
             arvore.deletar(numero);
         }
 
-        // Verificando o fator de balanceamento de cada nó após as remoções
         System.out.println("\nÁrvore após remoções:");
         arvore.imprimirFatoresBalanceamento();
     }
